061acba85183d11353c30c91382c03c2357ec96b
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2023-03-14 00:51:16 -0300

    updating locally configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,11 @@ require('@uniswap/eslint-config/load')
 
 module.exports = {
   extends: '@uniswap/eslint-config/react',
+  plugins: ['unused-imports'],
+  rules: {
+    'import/no-unused-modules': ['error', { unusedExports: true }],
+    'unused-imports/no-unused-imports': 'error',
+  },
 }
+
+
